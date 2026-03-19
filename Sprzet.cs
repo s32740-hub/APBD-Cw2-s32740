90229fe7e8f1d8452bcf61353b5525ea0b79171b
s32740 <s32740@pjwstk.edu.pl>
2026-03-19 21:42:47 +0100

    feat: add abstract Sprzet class and .gitignore configuration

---
+++ Sprzet.cs
@@ -0,0 +1,15 @@
+﻿public abstract class Sprzet
+{
+    private static int licznikSprzetu = 0;
+    public string name { get; set; };
+    public string status { get; set; };
+    public int sprzetID { get; private set; };
+
+    protected Sprzet(string name, string status)
+    {
+        this.name = name;
+        this.status = status;
+        this.sprzetID = licznikSprzetu++;
+    }
+}
+//feat: create abstract Sprzet class with auto-ID generation
