b7364403ce418aab02e6d1c276c41e3d0b043c97
s32740 <s32740@pjwstk.edu.pl>
2026-03-20 08:42:35 +0100

    feat: add abstract class Uzytkownik

---
+++ Uzytkownik.cs
@@ -0,0 +1,18 @@
+﻿namespace Wypozyczalnia;
+
+public abstract class Uzytkownik
+{
+    private static int LicznikUzytkownikow = 0;
+    public int UserId { get; private set; }
+    public string Name { get; }
+    public string Nazwisko { get; }
+    public string TypUzytkownika { get; protected set; }
+
+    public Uzytkownik(string name, string nazwisko, string typUzytkownika)
+    {
+        this.Name = name;
+        this.Nazwisko = nazwisko;
+        this.TypUzytkownika = typUzytkownika;
+        this.UserId = LicznikUzytkownikow++;
+    }
+}
