a82ce026b686400f201c43fe3f60527533e9c05a
s32740 <s32740@pjwstk.edu.pl>
2026-03-20 08:59:36 +0100

    feat: implement Student, Employee, Projector subclasses and UzytkownikManager

---
+++ Student.cs
@@ -0,0 +1,7 @@
+﻿namespace Wypozyczalnia;
+
+public class Student : Uzytkownik
+{
+    public Student(string name, string nazwisko) : 
+        base(name, nazwisko, "Student"){}
+}
---
+++ UzytkownikManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Wypozyczalnia;
+
+public class UzytkownikManager
+{
+    public List<Uzytkownik> Uzytkownicy { get; private set; } = new List<Uzytkownik>();
+
+    public void AddUzytkownik(Uzytkownik uzytkownik)
+    {
+        if (uzytkownik != null)
+        {
+            this.Uzytkownicy.Add(uzytkownik);
+        }
+    }
+
+    public void WyswietlUzytkownikow()
+    {
+        foreach (var u in Uzytkownicy)
+        {
+            Console.WriteLine($"{u.UserId}: {u.Name} {u.Nazwisko} ({u.TypUzytkownika})");
+        }
+    }
+}
